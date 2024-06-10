1f9b50ada63d91a96c55b3445b43ca61660f499d
Rohiteswargouthu <21131A4209@gvpce.ac.in>
2024-06-10 19:26:38 +0530

    adding new files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,19 @@
-import { Hono } from 'hono'
-import { userRouter } from './routes/user';
-import { blogRouter } from './routes/blog';
-import { cors } from 'hono/cors'
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { userRouter } from './routes/user.route';
+import { blogRouter } from './routes/blog.route';
+
 
 const app = new Hono<{
   Bindings: {
-    DATABASE_URL: string;
-    JWT_SECRET: string;
+    DATABASE_URL: string,
+    JWT_SECRET: string
   }
-}>();
-app.use('/*', cors())
-app.route("/api/v1/user", userRouter);
-app.route("/api/v1/blog", blogRouter);
+}>()
 
-export default app
+app.use("/*", cors())
+app.route("/api/v1/user", userRouter)
+app.route("/api/v1/blog", blogRouter)
+
+
+export default app;
